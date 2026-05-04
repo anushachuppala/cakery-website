db7594efd93f32dcdfa7130130d3409cf7fdd61f
anushachuppala <anusha.chuppala12@gmail.com>
2026-05-04 22:04:03 +0530

    added API

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const navlist = document.getElementById("navlist");
-const hamburger = document.getElementById("hamburger");
+document.addEventListener("DOMContentLoaded", () => {
+  const navlist = document.getElementById("navlist");
+  const hamburger = document.getElementById("hamburger");
 
-//click event
+  // hamburger menu
+  hamburger.addEventListener("click", () => {
+    navlist.classList.toggle("navlist-active");
+  });
 
-hamburger.addEventListener("click", () => {
-  navlist.classList.toggle("navlist-active");
+  // ORDER BUTTON
+  const orderBtn = document.getElementById("orderBtn");
+
+  orderBtn.addEventListener("click", () => {
+    fetch("http://localhost:3000/api")
+      .then((res) => res.json())
+      .then((data) => {
+        alert(data.message);
+      })
+      .catch((err) => {
+        console.log("Error:", err);
+        alert("API not working");
+      });
+  });
 });
